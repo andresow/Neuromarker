99e26f1edbdc0e35215ed6dc39907a41eaf696b3
andresow <andres.serrato@correounivalle.edu.co>
2019-07-18 04:40:18 -0500

    models comissions

--- nodes/models.py
+++ nodes/models.py
@@ -5,6 +5,7 @@ class Nodes(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     is_red = models.BooleanField()
     name  = models.TextField(max_length=500, blank=True)
+    percentage_commission = models.IntegerField()
 
 class Node_father(models.Model):
     
@@ -12,3 +13,10 @@ class Node_father(models.Model):
     initial_date = models.DateField()
     final_date = models.DateField()
 
+class Comission(models.Model):
+
+    node = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    value_commision = models.IntegerField()
+    type_payment = models.TextField(max_length=500, blank=True)
+    type_commission = models.TextField(max_length=500, blank=True)
+    current_balance = models.IntegerField(null=True)
